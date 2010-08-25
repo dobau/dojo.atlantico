e845b1d64e29a9b59db397303760f3c24834045a
root <root@atlantico2348l.(none)>
2010-08-25 17:02:14 -0300

    Adicionando o segundo desafio no github

---
+++ campominado/campominado.rb
@@ -0,0 +1,47 @@
+#
+# Desafio do campo minado
+#
+# @link http://www.programming-challenges.com/pg.php?page=downloadproblem&probid=110102&format=html
+# @author Rafael Alves, Arian Maykon e Wesley Jefferson
+# @since 24/08/2010
+class CampoMinado
+
+	attr_accessor :mina
+		
+	def initialize(x, y)
+		@mina = Array.new(x).map!{ Array.new(y) }
+	
+		for i in 0..x-1
+			for j in 0..y-1
+				@mina[i][j] = '.'
+			end
+		end
+	end
+	
+	def adicionaMina(x, y)
+		@mina[x][y] = '*'
+	end
+	
+	def processa()
+		@mina.each_index do |i|
+			@mina[i].each_index do |j|
+				processa_celula(i,j)
+			end
+		end
+	end
+
+	def processa_celula(i,j)
+		if (@mina[i][j] == '.')
+			@mina[i][j] = 0
+
+			@mina[i][j] += 1 if (j > 0 && @mina[i][j-1] == '*')
+			@mina[i][j] += 1 if (j+1 < @mina[i].length && @mina[i][j+1] == '*')
+
+			@mina[i][j] += 1 if (i > 0 && @mina[i-1][j] == '*')
+			@mina[i][j] += 1 if (i+1 < @mina.length && @mina[i+1][j] == '*')
+
+			@mina[i][j] = @mina[i][j].to_s
+		end
+	end
+
+end
---
+++ campominado/tdd.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require 'campominado'
+
+class Testador < Test::Unit::TestCase
+	
+	def test_1x1
+		campoMinado = CampoMinado.new(1,1)
+		campoMinado.adicionaMina(0,0)
+		minaProcessada = campoMinado.processa()
+		
+		assert_equal([['*']], minaProcessada)
+	end
+
+	def test_1x2
+		campoMinado = CampoMinado.new(1,2)
+		campoMinado.adicionaMina(0,1)
+		minaProcessada = campoMinado.processa()
+		
+		assert_equal([['1','*']], minaProcessada)
+	end	
+
+	def test_2x1
+		campoMinado = CampoMinado.new(2,1)
+		campoMinado.adicionaMina(0,0)
+		minaProcessada = campoMinado.processa()
+		
+		assert_equal([['*'],['1']], minaProcessada)
+	end
+
+	def test_2x2
+		campoMinado = CampoMinado.new(2,2)
+		campoMinado.adicionaMina(0,0)
+		minaProcessada = campoMinado.processa()
+		
+		assert_equal([['*', '1'],['1', '1']], minaProcessada)
+	end
+end
